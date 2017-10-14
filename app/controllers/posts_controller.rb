1436be56930ce0a9c6d9223deac3de112bcf41d1
spartakusQ <spartakus1688@gmail.com>
2017-10-14 20:47:28 +0300

    Add Posts controller & views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+ def new
+
+ end
+
+  def create
+  @post = Post.new(post_params)
+
+  @post.save
+  redirect_to @post
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
