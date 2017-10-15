d8285ebd8f9427f21cb11a595156ceed553ffb36
spartakusQ <spartakus1688@gmail.com>
2017-10-15 20:21:12 +0300

    Render new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,17 +8,18 @@ class PostsController < ApplicationController
    @post = Post.find(params[:id])
  end
 
-
-
- def new
-
- end
+  def new
+    @post = Post.new
+  end
 
   def create
-  @post = Post.new(post_params)
-  @post.save
-  render 'show'
+    @post = Post.new(post_params)
+  if @post.save
+    redirect_to @post
+  else
+    render 'new'
   end
+end
 
   private
   def post_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :author
+  validates :title, presence: true,
+                  length: { minimum: 5 }
 end
