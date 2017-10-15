b0af2a4b88360653b3d4e810dd3cde099a42aa25
spartakusQ <spartakus1688@gmail.com>
2017-10-16 01:17:48 +0300

    Add Comment delete functional & dependent option to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,13 @@
 class CommentsController < ApplicationController
-  def create    
+  def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
     redirect_to post_path(@post)
   end
 
