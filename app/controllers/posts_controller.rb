f197916a0af4ce9afb9a50d142712bbbce2cc132
spartakusQ <spartakus1688@gmail.com>
2017-10-15 23:34:09 +0300

    Add delete functional & some fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,40 +4,47 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def edit
-   @post = Post.find(params[:id])
-  end
-
   def show
    @post = Post.find(params[:id])
- end
+  end
 
   def new
     @post = Post.new
   end
 
+  def edit
+   @post = Post.find(params[:id])
+  end
+
   def create
     @post = Post.new(post_params)
-  if @post.save
-    redirect_to @post
-  else
-    render 'new'
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
-  end
   def update
-  @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
 
-  if @post.update(post_params)
-    redirect_to @post
-  else
-    render 'edit'
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to @post
   end
 
 
   private
-
   def post_params
     params.require(:post).permit(:title, :body)
   end
