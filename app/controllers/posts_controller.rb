6657911a92a504dd802dcaed5d65d175fb5161a9
spartakusQ <spartakus1688@gmail.com>
2017-10-15 19:11:25 +0300

    Link to welcome index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,12 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.all
+  end
+
   def show
    @post = Post.find(params[:id])
  end
-  def index
-    @post = Post.all
-  end
-
-
-
 
 
 
