7d54fb5be8342935a018028d759d4fedf6bed224
spartakusQ <spartakus1688@gmail.com>
2017-10-16 00:06:23 +0300

    Updated to model Comment

--- db/migrate/20171015210329_create_comments.rb
+++ db/migrate/20171015210329_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
+      t.string :commenter
       t.text :body
+      t.references :post, foreign_key: true
       t.datetime :published_at
 
       t.timestamps
