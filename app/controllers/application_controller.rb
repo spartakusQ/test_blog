eda70bdd4cbfaceb5012a20d030bb11b8c6ceca4
spartakusQ <spartakus1688@gmail.com>
2017-10-24 21:48:41 +0300

    Some fix form to name & fields to author

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,8 @@ class ApplicationController < ActionController::Base
 
   protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email])
+  end
 end
